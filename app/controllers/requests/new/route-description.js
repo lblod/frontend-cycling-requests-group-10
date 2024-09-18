934ab9adc1ef2be4a47e32847358ff43bbc5ab7f
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2024-09-18 10:34:38 +0200

    feat: Add navigation actions for next and back steps in route description

--- app/controllers/requests/new/route-description.js
+++ app/controllers/requests/new/route-description.js
@@ -3,11 +3,17 @@ import { action } from '@ember/object';
 
 export default class RequestsNewRouteDescriptionController extends Controller {
   nextStep = 'requests.new.extra-info';
+  prevStep = 'requests.new.identity';
 
   @action
-  onSubmit() {
-    console.log('onSubmit', arguments);
-    console.log('Submitting form...');
+  onNext() {
+    console.log('onNext', arguments);
     this.transitionToRoute(this.nextStep);
+  }
+
+  @action
+  onBack() {
+    console.log('onBack', arguments);
+    this.transitionToRoute(this.prevStep);
   }
 }
